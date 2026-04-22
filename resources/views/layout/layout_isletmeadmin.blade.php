451e2097cc9b92ac36971913a56ade7d3e2e3221
ferdi <webfirmam1035@gmail.com>
2026-04-22 23:10:42 +0300

    Çarkıfelek menüsü görünür yapıldı (display:none kaldırıldı)

--- resources/views/layout/layout_isletmeadmin.blade.php
+++ resources/views/layout/layout_isletmeadmin.blade.php
@@ -977,7 +977,7 @@
                         ><span class="mtext">Ajanda</span>
                      </a>
                   </li>
-                      <li style="display:none;">
+                      <li>
                      @if($pageindex==500)
                      <a href="/isletmeyonetim/carkifelek{{(isset($_GET['sube'])) ? '?sube='.$isletme->id : '' }}" class="dropdown-toggle no-arrow active">
                      @else
