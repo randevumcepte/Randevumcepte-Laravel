08781f565591154b5e1e259608bc97dd910ff559
ferdi <webfirmam1035@gmail.com>
2026-05-17 04:56:57 +0300

    Revert "Musteri yorum/puan panosu — sidebar + anasayfa karti"
    This reverts commit 16c8318babf2e84c59ca79f87f062f4d42082e00.

--- routes/web.php
+++ routes/web.php
@@ -970,9 +970,6 @@ Route::prefix('isletmeyonetim')->group(function() {
 	Route::post('/google-review-kaydet','StoreAdminController@googleReviewKaydet');
 	Route::post('/reputation-premium-ac','StoreAdminController@reputationPremiumAc');
 
-	// Musteri yorumlari + puanlari (salon yorum panosu)
-	Route::get('/musteri-yorumlari','StoreAdminController@musteriYorumlari')->name('isletmeadmin.musteriYorumlari');
-
 	Route::post('/sozlesme-olustur','StoreAdminController@sozlesmeOlustur');
 	Route::get('/formmusteribilgigetir','StoreAdminController@formmusteribilgigetir');
 	Route::get('/formpersonelbilgigetir','StoreAdminController@formpersonelbilgigetir');
